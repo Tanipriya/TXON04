86c643ece7030cc5b1967519428e3498475e82a3
tanisha.priya007@gmail.com <tanisha.priya007@gmail.com>
2023-02-03 23:15:16 +0530

    final submission

---
+++ script/index.js
@@ -0,0 +1,11 @@
+let typed = new Typed(".typing", {
+    strings: [ "",
+      " Tanisha Priya",
+      " Web Devloper",
+      " Web Designer",
+      " Frontend Developer.",
+    ],
+    typeSpeed: 150,
+    backSpeed: 80,
+    loop: true,
+  });
